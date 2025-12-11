a5eebef77f0dd853a5079cbc1a7ba6bfe4d6ac3e
312Evan <emcgrael@outlook.com>
2025-12-11 10:20:54 -0500

    config update

--- src/main/java/frc/robot/swerve/Module.java
+++ src/main/java/frc/robot/swerve/Module.java
@@ -17,7 +17,6 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.Volts;
 
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
-import frc.robot.subsystems.Swerve;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
